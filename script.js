b77121f09ef96301187ce039dd270e335ae3b986
Rüya <68228951+ruyav44@users.noreply.github.com>
2024-06-01 13:56:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    // Fetch the password from the external site
-    let response = await fetch('https://paste.tc/raw/ruyapanel');
-    let correctPassword = await response.text().trim();
+document.addEventListener('DOMContentLoaded', () => {
+    const correctPassword = "ruyav44";
     
+    // Play the background audio once
+    const backgroundAudio = document.getElementById('background-audio');
+    backgroundAudio.play();
+
     // Handle the form submission
     document.getElementById('login-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        let enteredPassword = document.getElementById('password').value;
-        let errorMessageElement = document.getElementById('error-message');
+        const enteredPassword = document.getElementById('password').value;
+        const errorMessageElement = document.getElementById('error-message');
         
         if (enteredPassword === correctPassword) {
-            let correctAudio = new Audio('ses/ruyav44dogru.mp3');
+            const correctAudio = new Audio('ses/ruyav44dogru.mp3');
             correctAudio.play();
             setTimeout(() => {
                 window.location.href = '/ruyasunucum/index.html';
             }, 6000);
         } else {
-            let incorrectAudio = new Audio('ses/ruyav44yanlis.mp3');
+            const incorrectAudio = new Audio('ses/ruyav44yanlis.mp3');
             incorrectAudio.play();
             errorMessageElement.innerText = "You Entered The Wrong Password. Our Telegram Address For Help: t.me/ruyav44";
             errorMessageElement.style.display = 'block';
