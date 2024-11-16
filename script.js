038a7dde87f22860b51bb784f640a8f7f95e4699
Rüya <68228951+ruyav44@users.noreply.github.com>
2024-11-16 19:33:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctPassword = "adminpw123";
+    const correctPassword = "adminpw123"; // amk şifresini crackleyemeninde amk 
     
     // Handle the form submission
     document.getElementById('login-form').addEventListener('submit', function(event) {
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const errorMessageElement = document.getElementById('error-message');
         
         if (enteredPassword === correctPassword) {
-            const correctAudio = new Audio('ses/dogru.mp3');
+            const correctAudio = new Audio('ses/yanlis.mp3'); // sesler karıştı aq WQEQWEQWE
             correctAudio.play();
             setTimeout(() => {
                 window.location.href = 'https://ruyav44.github.io/ruyaminecraft/bomb/bomb.html';
             }, 6000);
         } else {
-            const incorrectAudio = new Audio('ses/yanlis.mp3');
+            const incorrectAudio = new Audio('ses/dogru.mp3'); // sesler karıştı aq WQEQWEQWE
             incorrectAudio.play();
             errorMessageElement.innerText = "You Entered The Wrong Password. Try Again Later!";
             errorMessageElement.style.display = 'block';
